08359db1607b6a3d556ade25617b754e49c92a0d
Eric M <evm2556@gmail.com>
2017-04-29 12:17:38 -0400

    adding readme. cleaning directory.

--- protractorTest/offutt-spec.js
+++ protractorTest/offutt-spec.js
@@ -1,14 +1,16 @@
-describe('click around nonAngular', function () {
+describe('click around offutt site', function () {
     beforeEach(function () {
         return browser.ignoreSynchronization = true;
     });
 
-    it('access 737', function () {
+    it('should have links for schedules and syllabus', function () {
         browser.get('http://cs.gmu.edu/~offutt/');
-        browser.wait(ExpectedConditions.visibilityOf(element(by.linkText('737'))));
+        //browser.wait(ExpectedConditions.visibilityOf(element(by.linkText('737'))));
         element(by.linkText('737')).click();
-        browser.wait(ExpectedConditions.visibilityOf(element(by.linkText('Schedule'))));
+        //browser.wait(ExpectedConditions.visibilityOf(element(by.linkText('Schedule'))));
         element(by.linkText('Schedule')).click();
-        browser.wait(ExpectedConditions.visibilityOf(element(by.linkText('Schedule'))));
+        //browser.wait(ExpectedConditions.visibilityOf(element(by.linkText('Syllabus'))));
+        element(by.linkText('Syllabus')).click();
+        browser.sleep(2000);
     });
 });
---
+++ readme.txt
@@ -0,0 +1,16 @@
+Showcase for testing E2E frameworks Protractor, Nightwatch.JS and Webdriver.IO. 
+
+These steps will install the dependencies for this project globally and run them from their directories. For real projects you will want to setup a local repository (package.json/node_modules) and install on a per project basis. 
+
+1. Install latest node from site. 
+2. Ensure npm is on path. 
+3. Install webdriver-manager globally. 
+    -npm install -g webdriver-manager
+4. Install protractor, nightwatch, and webdriverio globally. 
+
+5. To run protractor, enter the protractorTest folder and run
+    > protractor conf.js
+6. To run webdriverio, enter the webdriverioTest folder and run
+    > wdio
+7. To run webdriverio, enter the webdriverioTest folder and run
+    > nightwatch
